d35f2ce56844809a163bf35eb86566658ec823e3
OmarAlbader <oabader21@cit.just.edu.jo>
2025-02-17 22:11:11 +0300

    form validation
    ALWAYS Validate, NEVER Trust the User.

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -4,7 +4,8 @@
     </x-slot:heading>
 
     <form method="POST" action="/jobs">
-        @csrf {{-- In vanilla php, it compiles to <input type="hidden" name="_token" value="TOKEN_VALUE_HERE" autocomplete="off"> --}}
+        @csrf
+        <!-- In vanilla php, it compiles to <input type="hidden" name="_token" value="TOKEN_VALUE_HERE" autocomplete="off"> -->
 
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
@@ -20,8 +21,13 @@
                                 class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focusing-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                 <input type="text" name="title" id="title"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="Shift Leader">
+                                    placeholder="Shift Leader" required>
+
                             </div>
+
+                            @error('title')
+                                <p class='text-xs text-red-500 font-semibold mt-1'>{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
 
@@ -32,18 +38,32 @@
                                 class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focusing-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                 <input type="text" name="salary" id="salary"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="$50,000 Per Year">
+                                    placeholder="$50,000 Per Year" required>
                             </div>
+
+                            @error('salary')
+                                <p class='text-xs text-red-500 font-semibold mt-1'>{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
                 </div>
+
+                {{-- <div class="mt-10">
+                    @if ($errors->any())
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li class='text-red-500 italic'>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                </div> --}}
             </div>
         </div>
 
 
         </div>
 
-        <div class="m-6 flex items-center justify-end gap-x-6">
+        <div class="mr-24 flex items-center justify-end gap-x-6">
             <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
             <button type="submit"
                 class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
