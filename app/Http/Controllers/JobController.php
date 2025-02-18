0d58397d1ec13bee78b0d3c1f6429fc5a5c3521e
OmarAlbader <oabader21@cit.just.edu.jo>
2025-02-18 17:11:52 +0300

    Routes tips
    1- Route model binding
    2- Dedicated controller classes
    3- Route::view()
    4- List your routes
    5- Route groups
    6- Route resources ( index, show, create, store, edit, update, destroy )

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    public function index()
+    {
+        $jobs = Job::with('employer')->latest()->simplePaginate(3);   // Eager loading, to prevent lazy loading and alleviate N+1 query problem. (see https://laravel.com/docs/8.x/eloquent-relationships#eager-loading)
+
+        return view('jobs.index', [
+            'jobs' => $jobs
+        ]);
+    }
+
+    public function create()
+    {
+        return view('jobs.create');
+    }
+
+    public function show(Job $job)
+    {
+        return view('jobs.show', ['job' => $job]);
+    }
+
+    public function store()
+    {
+        request()->validate([
+            'title' => ['required', 'min: 3'],
+            'salary' => ['required'],
+        ]);
+
+        Job::create([
+            'title' => request('title'),
+            'salary' => request('salary'),
+            'employer_id' => 1,
+        ]);
+
+        return redirect('/jobs');
+    }
+
+    public function edit(Job $job)
+    {
+        return view('jobs.edit', ['job' => $job]);
+    }
+
+    public function update(Job $job)
+    {
+        // authorize (On hold...)
+
+        request()->validate([
+            'title' => ['required', 'min: 3'],
+            'salary' => ['required'],
+        ]);
+
+        $job->update([
+            'title' => request('title'),
+            'salary' => request('salary'),
+        ]);
+
+        return redirect('/jobs/' . $job->id);
+    }
+
+    public function destroy(Job $job)
+    {
+        // authorize (On hold...)
+
+        // delete the job
+        $job->delete();
+
+        // redirect
+        return redirect('/jobs');
+    }
+}
